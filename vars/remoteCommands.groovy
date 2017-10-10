466dcc5c0a54f7d2ea2f7b9360cb15950b3bf181
Jason DeTiberus <jdetiber@redhat.com>
2017-10-09 22:30:02 -0400

    attempt without pty allocation

--- vars/remoteCommands.groovy
+++ vars/remoteCommands.groovy
@@ -16,7 +16,7 @@ ${cmd}
 SCRIPT
               chmod +x "${script}"
               scp ${ssh_opts} "${script}" root@${test_hostname}:"${script}"
-              ssh -t ${ssh_opts} root@${test_hostname} "bash -l -c \"timeout 14400 ${script}\""
+              ssh ${ssh_opts} root@${test_hostname} "bash -l -c \"timeout 14400 ${script}\""
            ''')
       }
     }
